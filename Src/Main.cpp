0cbdc33870367ba81b726cf60a3def98734d5828
Mohammad Elkhouly <mohammad4elkhouly@gmail.com>
2026-03-18 18:49:54 +0200

    V0.2
    GLFW Initialization successful Learned the basics of GLFW

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -1,21 +1,34 @@
-#include <iostream>
-#include <cmath>
 #include <GLFW/glfw3.h>
 
-#define EarthGravityConstant 9.8
-const int FallHeight = 100; // Meters  //* body fall height 100 meters.
-/*
---------------------------------------------------
- Newtons Second Law
-    a = F/m Acceleration = (Gravity Force) / mass
---------------------------------------------------
-*/
+#include <iostream>
 
-int main(){
-  double Acceleration = EarthGravityConstant; // m/s meter per second
-  std::cout << Acceleration << " Per second" << std::endl;
-  double Time = sqrt((2 * FallHeight) / Acceleration);
+int main(void) {
+  GLFWwindow* Window;
 
-  std::cout << "Time to reach ground: " << Time << " seconds" << std::endl;
+  if (!glfwInit()) {
+    std::cerr << "Error while Initializing GLFW";
+    return -1;
+  }
+
+  Window = glfwCreateWindow(1366, 768, "ProjectOrbitV0.3", NULL, NULL);
+  if (!Window) {
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(Window);
+
+  // glColor4f(0.56078f, 0.85098f, 0.98431f, 1.0f);
+  // GLFW Properties
+  glClearColor(0.56078f, 0.85098f, 0.98431f, 1.0f);
+
+  while (!glfwWindowShouldClose(Window)) {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glfwSwapBuffers(Window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+
   return 0;
 }
